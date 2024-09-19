5e938067431a3212edd33afef224ce70712b3852
ilijaradojkovic <radojkovicika@gmail.com>
2024-09-19 12:55:04 +0200

    added @EnableDiscoveryClient

--- src/main/java/bees/io/Currency/CurrencyApplication.java
+++ src/main/java/bees/io/Currency/CurrencyApplication.java
@@ -2,10 +2,13 @@ package bees.io.Currency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
+
 public class CurrencyApplication {
 
 	public static void main(String[] args) {
